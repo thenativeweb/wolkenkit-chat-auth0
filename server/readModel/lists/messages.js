fc53d7e1df99abd5f633c5f746a1c3ca2a4967a6
Golo Roden <golo.roden@thenativeweb.io>
2018-01-14 14:17:23 +0100

    Update messages.js

--- server/readModel/lists/messages.js
+++ server/readModel/lists/messages.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const fields = {
+  timestamp: { initialState: 0, fastLookup: true },
   author: { initialState: '' },
   text: { initialState: '' },
-  likes: { initialState: 0 },
-  timestamp: { initialState: 0, fastLookup: true }
+  likes: { initialState: 0 }
 };
 
 const when = {
-  'communication.message.sent': (messages, event, mark) => {
+  'communication.message.sent' (messages, event, mark) {
     messages.add({
       author: event.data.author,
       text: event.data.text,
@@ -17,7 +17,7 @@ const when = {
     mark.asDone();
   },
 
-  'communication.message.liked': (messages, event, mark) => {
+  'communication.message.liked' (messages, event, mark) {
     messages.update({
       where: { id: event.aggregate.id },
       set: {
